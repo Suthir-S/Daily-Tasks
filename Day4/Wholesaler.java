048a749acb0f44a0feb389d94292063721e42ec1
Suthir-S <69359674+Suthir-S@users.noreply.github.com>
2023-01-17 09:49:53 +0530

    Delete Wholesaler.java

--- Day4/Wholesaler.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProductManagement.interfaces;
-
-import com.example.ProductManagement.models.Wholesalers;
-import org.springframework.stereotype.Service;
-
-@Service("WholesalerDetails")
-public interface Wholesaler {
-    Wholesalers DisWholesalers(int wid);
-}
